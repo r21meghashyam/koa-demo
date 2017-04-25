4c3c57840a229f41f7489bab0c05a4f6e65ebf43
Meghashyam Bhandary <Meghashyam Bhandary>
2017-04-25 19:43:34 +0530

    On request

--- server/chokidar.js
+++
@@ -1,5 +0,0 @@
-var chokidar = require('chokidar');
-// One-liner for current directory, ignores .dotfiles
-chokidar.watch(['./server'], {}).on('all', (event, path) => {
-    console.log(event, path);
-});
--- server/main.js
+++ server/main.js
@@ -1,14 +1,37 @@
+const path = require('path');
 const Koa = require('koa');
+const decache = require('decache');
 const app = new Koa();
 const port = 3000;
+let devMode = true;
 
-require('./chokidar.js');
+if (devMode) {
+    var chokidar = require('chokidar')
+    var watcher = chokidar.watch('./server')
 
+    watcher.on('all', function(event, _path) {
+        console.log(event, _path);
+        let file = path.join(__dirname, '../', _path)
+
+        if (event == 'change') {
+            Object.keys(require.cache).forEach(function(id) {
+                if (file == id) {
+                    console.log(1, typeof(require.cache[id]));
+                    decache(id);
+                    console.log(2, typeof(require.cache[id]));
+                    delete require.cache[id];
+                    console.log(3, typeof(require.cache[id]));
+                }
+            })
+        }
+    })
+
+}
 app.use(ctx => {
-
     if (ctx.url != '/')
         ctx.response.redirect('/')
-    ctx.body = 'Hello World';
+
+    ctx.body = 'Hello world';
 });
 
 app.listen(port);
